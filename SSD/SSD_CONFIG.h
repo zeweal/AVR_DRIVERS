cc02724cc2a6cd17789e4c5c4eefcd64fafd5f88
zeweal <160158316+zeweal@users.noreply.github.com>
2025-05-11 03:03:26 +0300

    Create SSD_CONFIG.h

---
+++ SSD/SSD_CONFIG.h
@@ -0,0 +1,12 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    SSD_CONFIG.h    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author : Abdallah Zeweal
+ *  Layer  : HAL
+ *  SWC    : SSD
+ *
+ */
+#ifndef SSD_CONFIG_H_
+#define SSD_CONFIG_H_
+
+#endif
