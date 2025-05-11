7fe1093836e598066981a16d8c199dc3f86b6434
zeweal <160158316+zeweal@users.noreply.github.com>
2025-05-11 03:08:43 +0300

    Create GIE_CONFIG.h

---
+++ GIE/GIE_CONFIG.h
@@ -0,0 +1,12 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    GIE_CONFIG.h    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author : Abdallah Zeweal
+ *  Layer  : HAL
+ *  SWC    : GIE
+ *
+ */
+#ifndef GIE_CONFIG_H_
+#define GIE_CONFIG_H_
+
+#endif
